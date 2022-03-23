20dfc0621fc83bd9c722702db0d120b14d468d07
BergeDios <santiago.goyret@gmail.com>
2022-03-23 09:50:03 -0700

    task 1 fixing correct handle of insertion

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,12 +14,15 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	new_node->n = value;
 	new_node->parent = parent;
-	new_node->left = parent->left;
+	new_node->left = NULL;
 	new_node->right = NULL;
 	if (parent)
 	{
 		if (parent->left)
+		{
 			parent->left->parent = new_node;
+			new_node->left = parent->left;
+		}	
 		parent->left = new_node;
 	}
 	return (new_node);
