c49d96c60b4eef28f55e59f51bae757c290f19ef
BergeDios <santiago.goyret@gmail.com>
2022-03-23 10:15:32 -0700

    task 1 and 2 fix betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -22,7 +22,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		{
 			parent->right->parent = new_node;
 			new_node->tight = parent->right;
-		}	
+		}
 		parent->right = new_node;
 	}
 	else
