853432b9a9328db9b6ca7c4dd5967f559e3c4edf
BergeDios <santiago.goyret@gmail.com>
2022-03-23 10:22:49 -0700

    task 17 change == to directions (&)

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,7 +8,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
 	if (!node || !node->parent)
 		return (NULL);
-	if (node->parent->right->n == node->n)
+	if (&node->parent->right == &node)
 		return (node->parent->left);
 	else
 		return (node->parent->right);
